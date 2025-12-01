1f25eeed06e33803f8dd2581ba10eed7faa10c9d
xProSergi <sergioinstituto21.22@gmail.com>
2025-12-01 18:14:21 +0100

    Sombreado

--- app.py
+++ app.py
@@ -271,6 +271,7 @@ def render_hero():
                     right: 0;
                     bottom: 0;
                     left: 0;
+                    background-color: rgba(0, 0, 0, 0.4); /* Capa negra semi-transparente */
                     z-index: 0;
                 }}
 
